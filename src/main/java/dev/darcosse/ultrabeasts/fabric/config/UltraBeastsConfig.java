1c1c031e2b473e2e2fdb2995e4b3007231525e54
Bilel <bsahraoui.mail@gmail.com>
2026-01-30 01:36:39 +0100

    Increased the chance for a Wormhole to spawn

--- src/main/java/dev/darcosse/ultrabeasts/fabric/config/UltraBeastsConfig.java
+++ src/main/java/dev/darcosse/ultrabeasts/fabric/config/UltraBeastsConfig.java
@@ -1,6 +1,6 @@
 package dev.darcosse.ultrabeasts.fabric.config;
 
 public class UltraBeastsConfig {
-    public int WORMHOLE_SPAWN_CHANCE = 3250;
+    public int WORMHOLE_SPAWN_CHANCE = 2500;
     public int TRY_SPAWN_INTERVAL = 400;
 }
